5e1e8fe41be7e2de3a17318f83b4685384e682cc
virajkushwaha <virajkushwaha21@gmail.com>
2025-02-24 13:02:23 +0530

    [Viraj] Add .Adding UC2_MessageApp By giving query in url

--- src/main/java/com/messageapp/MessageApp/controller/MessageAppController.java
+++ src/main/java/com/messageapp/MessageApp/controller/MessageAppController.java
@@ -1,15 +1,22 @@
-package controller;
+package com.messageapp.MessageApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class MessageAppController {
     //UC1
     @GetMapping("/hello")
-    public String sayhello(){
+    public String sayhello() {
         return "Hello From BridgeLabz";
     }
-   
+    //UC2
+    @GetMapping("/query")
+    public String helloWithQuery(@RequestParam String name){
+        return "Hello " + name + " from BridgeLabz";
+    }
 }
+
