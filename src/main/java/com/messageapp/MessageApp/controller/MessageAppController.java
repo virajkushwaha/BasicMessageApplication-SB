f871a4507ce967d9abc57a8fbcba7760f94f22ff
virajkushwaha <virajkushwaha21@gmail.com>
2025-02-24 13:25:28 +0530

    [Viraj] Add .Adding UC2_MessageApp Post

--- src/main/java/com/messageapp/MessageApp/controller/MessageAppController.java
+++ src/main/java/com/messageapp/MessageApp/controller/MessageAppController.java
@@ -1,24 +1,33 @@
 package com.messageapp.MessageApp.controller;
 
 import org.springframework.web.bind.annotation.*;
+import com.messageapp.MessageApp.dto.UserDTO;
 
 @RestController
-
+@RequestMapping("/hello") // Base URL for all endpoints
 public class MessageAppController {
-    //UC1
-    @GetMapping("/hello")
-    public String sayhello() {
-        return "Hello From BridgeLabz";
+
+    // UC 1: Basic GET Request
+    @GetMapping
+    public String sayHello() {
+        return "Hello from BridgeLabz";
     }
-    //UC2
+
+    // UC 2: GET Request with Query Parameter
     @GetMapping("/query")
-    public String helloWithQuery(@RequestParam String name){
+    public String helloWithQuery(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
     }
+
     // UC 3: GET Request with Path Variable
     @GetMapping("/param/{name}")
     public String sayHelloWithPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
-}
 
+    // UC 4: POST Request with JSON Body
+    @PostMapping("/post")
+    public String printingJsonData(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
+    }
+}
